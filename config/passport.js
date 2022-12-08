5442bf17255d0206f7126f21b021d55826f085d8
wolfofwsb <aidansery@gmail.com>
2022-12-08 14:30:55 -0600

    set up oauth

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,47 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
-
+const User = require('../models/user')
 // configuring Passport!
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  function(accessToken, refreshToken, profile, cb) {
+  async function(accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
     // refer to the lesson plan from earlier today in order to set this up
-
+    let user = await User.findOne({ googleId: profile.id });
+    // Existing user found, so provide it to passport
+    if (user) return cb(null, user);
+    // We have a new user via OAuth!
+    // When using async/await to consume promises,
+    // there is no use of .then or .catch, so we
+    // use a try/catch block to handle an error
+    try {
+      user = await User.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      });
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
   }
 ));
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  done(null, user._id);
 });
 
-passport.deserializeUser(function(id, done) {
-
+passport.deserializeUser(function(userId, done) {
+  User.findById(userId).then(function(user){
+     done(null, user);
+  })
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
 
 });
-
-
-
--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/oauth2callback', passport.authenticate(
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/'). // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
